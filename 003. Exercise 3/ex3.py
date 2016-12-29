929e1fa25a6633df390af2e4e4a0de819a04b64e
enemy123456789 <acmahaja@gmail.com>
2016-12-28 19:54:04 -0500

    Add files via upload

---
+++ 003. Exercise 3/ex3.py
@@ -0,0 +1,33 @@
+#String of text to describe the types of chickens and their respective population that succeeds it
+print "I will now count my chickens:"
+
+#A list of Hens and Roosters and their respective amounts. The amounts are in forumulas instead of their orginial numbers.
+print "Hens", 25 + 30 / 6
+print "Roosters", 100 - 25 * 3 % 4
+
+#This is a string of text to describe the number of eggs produced
+print "Now I will count the eggs:"
+
+#This is a formula to calculate the number of eggs produced. Note "4 % 2" does nothing
+print 3 + 2 + 1 - 5 + 4 % 2 - 1 / 4 + 6
+
+#This is a string descrbing 3 + 2 < 5 - 7?
+print "Is it true that 3 + 2 < 5 - 7?"
+
+#Output of the solution to the preciding text
+print 3 + 2 < 5 -7
+
+#This is a string of text and the solution of the output
+print "What is 3 + 2?", 3 + 2
+print "What is 5 - 7?", 5 - 7
+
+#String of text describing preciding text
+print "Oh, that's why it's False."
+
+#String of text describing succeding text
+print "How about some more."
+
+#This a string of text and their solutions in formula.
+print "Is it greater?", 5 > -2
+print "Is it greater or equal?", 5 >= -2
+print "Is it less or equal?", 5 <= -2
